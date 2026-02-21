b0692efc7274531258b666684aca52023847ed8c
almosistore-droid <almosistore@gmail.com>
2026-02-21 23:46:00 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -210,12 +210,18 @@ def main_handler(message):
     
     elif text == BTN_ADDRESS:
 
-    markup = types.InlineKeyboardMarkup(row_width=1)
+        markup = types.InlineKeyboardMarkup(row_width=1)
 
-    markup.add(
-        types.InlineKeyboardButton("🏢 Dushanbe", callback_data="city_dushanbe"),
-        types.InlineKeyboardButton("🏢 Bokhtar", callback_data="city_bokhtar"),
-        types.InlineKeyboardButton("🏢 Jabbor Rasulov", callback_data="city_jabbor")
+        markup.add(
+            types.InlineKeyboardButton("🏢 Dushanbe", callback_data="city_dushanbe"),
+            types.InlineKeyboardButton("🏢 Bokhtar", callback_data="city_bokhtar"),
+            types.InlineKeyboardButton("🏢 Jabbor Rasulov", callback_data="city_jabbor")
+    )
+
+    bot.send_message(
+        chat_id,
+        "📍 Шаҳрро интихоб кунед:",
+        reply_markup=markup
     )
 
     bot.send_message(
